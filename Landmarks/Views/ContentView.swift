5af862b85d79dfae5f651901318c336fd9d54aa8
Hercules <hercosmith03@gmail.com>
2021-08-31 12:22:44 +0200

    Essentials - Building Lists and Navigation

---
+++ Landmarks/Views/ContentView.swift
@@ -0,0 +1,20 @@
+//
+//  ContentView.swift
+//  Landmarks
+//
+//  Created by Hercules Smith on 2021/08/31.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        LandmarkList()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
