52dfaa342d19388c3849525e95c910eb3d4e1a02
Hercules <hercosmith03@gmail.com>
2021-08-31 14:37:53 +0200

    App Design & Layout - Composing Complex Interfaces

---
+++ Landmarks/Views/Categories/CategoryHome.swift
@@ -0,0 +1,41 @@
+//
+//  CategoryHome.swift
+//  Landmarks
+//
+//  Created by Hercules Smith on 2021/08/31.
+//
+
+import SwiftUI
+
+struct CategoryHome: View {
+    @EnvironmentObject var modelData: ModelData
+    
+    var body: some View {
+        NavigationView {
+            List {
+                modelData.features[0].image
+                    .resizable()
+                    .scaledToFill()
+                    .frame(height: 150)
+                    .clipped()
+                    .listRowInsets(EdgeInsets())
+                
+                ForEach(modelData.categories.keys.sorted(), id: \.self) { key in
+                    CategoryRow(
+                        categoryName: key,
+                        items: modelData.categories[key]!
+                    )
+                }
+                .listRowInsets(EdgeInsets())
+            }
+            .navigationTitle("Featured")
+        }
+    }
+}
+
+struct CategoryHome_Previews: PreviewProvider {
+    static var previews: some View {
+        CategoryHome()
+            .environmentObject(ModelData())
+    }
+}
